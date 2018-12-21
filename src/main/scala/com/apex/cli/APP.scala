a8bd94ff01ccfc281c7ddc6eda639b9505af3900
Artem Eger <11612812+yuomii@users.noreply.github.com>
2018-12-21 16:49:57 +0100

    Update APP.scala

--- src/main/scala/com/apex/cli/APP.scala
+++ src/main/scala/com/apex/cli/APP.scala
@@ -10,9 +10,7 @@ object APP {
       val privKey = Wallet.Default.getPrivKey()
       val account = RPC.post("showaccount", s"""{"address":"${privKey.publicKey.address}"}""")
       var nextNonce: Long = 0
-      if (account != JsNull) {
-        nextNonce = (account \ "nextNonce").as[Long]
-      }
+      nextNonce = (account \ "nextNonce").as[Long]
       var nonce1 = nextNonce + 1
       var nonce2 = nonce1 + 1
       Command.execute("sendrawtransaction -privkey 8d3d3ff1ebf1141000e9d844a3920a3bd4377f9bc5a4151b6eb30367785658ea -address AP9x3wKTf6GsdbuHa47MmTXdBvv54sAErkx -assetId 0000000000000000000000000000000000000000000000000000000000000000 -amount 0.001 -nonce "+nextNonce.toString)
