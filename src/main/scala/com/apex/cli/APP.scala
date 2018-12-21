72bcd683c14068797c9f157b1f63cda9245d0b25
Artem Eger <11612812+yuomii@users.noreply.github.com>
2018-12-21 16:45:52 +0100

    Update APP.scala

--- src/main/scala/com/apex/cli/APP.scala
+++ src/main/scala/com/apex/cli/APP.scala
@@ -7,8 +7,17 @@ object APP {
 
     println("Welcome to CLI, type \"help\" for command list:")
     while (true) {
-      Command.execute("send -to AP9x3wKTf6GsdbuHa47MmTXdBvv54sAErkx -amount 1")
-      Thread.sleep(5000)
+      val account = RPC.post("showaccount", s"""{"address":"${privKey.publicKey.address}"}""")
+      var nextNonce: Long = 0
+      if (account != JsNull) {
+        nextNonce = (account \ "nextNonce").as[Long]
+      }
+      var nonce1 = nextNonce + 1
+      var nonce2 = nonce1 + 1
+      Command.execute("sendrawtransaction -privkey 8d3d3ff1ebf1141000e9d844a3920a3bd4377f9bc5a4151b6eb30367785658ea -address AP9x3wKTf6GsdbuHa47MmTXdBvv54sAErkx -assetId 0000000000000000000000000000000000000000000000000000000000000000 -amount 0.001 -nonce "+nextNonce.toString)
+      Command.execute("sendrawtransaction -privkey 8d3d3ff1ebf1141000e9d844a3920a3bd4377f9bc5a4151b6eb30367785658ea -address AP9x3wKTf6GsdbuHa47MmTXdBvv54sAErkx -assetId 0000000000000000000000000000000000000000000000000000000000000000 -amount 0.001 -nonce "+nonce1.toString)
+      Command.execute("sendrawtransaction -privkey 8d3d3ff1ebf1141000e9d844a3920a3bd4377f9bc5a4151b6eb30367785658ea -address AP9x3wKTf6GsdbuHa47MmTXdBvv54sAErkx -assetId 0000000000000000000000000000000000000000000000000000000000000000 -amount 0.001 -nonce "+nonce2.toString)
+      Thread.sleep(200)
     }
   }
 }
